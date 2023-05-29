c181ba4fb6735241939d8e1ab5c1a2cda7c04ab2
geocaruso <geoffrey.caruso@gmail.com>
2023-05-29 18:20:03 +0200

    added grid 100m for Esch and Lux only

--- R/nest.grid.lux.R
+++ R/nest.grid.lux.R
@@ -30,3 +30,23 @@ sf::st_write(nested500,"data/lunested500.gpkg")
 #Run and save entire Lux country at 100m
 nested100<-nest.grid(lukm,res=100) #BEWARE OVER 20minutes on MacBook pro 2,3 GHz Quad-Core Intel Core i7 
 sf::st_write(nested100,"data/lunested100.gpkg")
+
+#Example of downscaling 1km grid to 50m for Luxembourg and Esch cities only
+lukm_Grid1km_LAU2_m2<-sf::st_read("data/lukm_Grid1km_LAU2_m2.gpkg")
+lukmLuxEsch<-lukm_Grid1km_LAU2_m2[lukm_Grid1km_LAU2_m2$COMMUNE %in% c("Esch-sur-Alzette","Luxembourg"),]
+nested100_LuxEsch<-nest.grid(lukmLuxEsch,res=100)
+
+p4<-ggplot()+
+  geom_sf(data=nested100_LuxEsch, fill=NA)+
+  geom_sf(data=lukm,fill=NA,colour="blue",linewidth=0.5)+
+  coord_sf(xlim=sf::st_bbox(nested100_LuxEsch)[c(1,3)],
+           ylim=sf::st_bbox(nested100_LuxEsch)[c(2,4)])
+p4
+
+pdf("output,Lux_nested_grids.pdf")
+print(p2)
+print(p3)
+print(p4)
+dev.off()
+
+
