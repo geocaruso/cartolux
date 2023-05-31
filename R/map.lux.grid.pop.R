8d3b8afdc3c5ede3cfd8cff401b4ad6859be6803
geocaruso <geoffrey.caruso@gmail.com>
2023-05-31 11:42:54 +0200

    urban share contribution coded. Map of inverse net density (m2 per person)

--- R/map.lux.grid.pop.R
+++ R/map.lux.grid.pop.R
@@ -38,43 +38,66 @@ source("R/ggplot.themap.R")
 
 thd<-0 #threshold for no population
 p0fi<-ggplot.themap(Grid1km_LAU2_popEU[Grid1km_LAU2_popEU$Pop2021EU>thd,],
-                     "Pop2021EU",n=7, style="fisher")
+                     "Pop2021EU",n=7, style="fisher",
+                    cl.colours = rev(RColorBrewer::brewer.pal(7,"Spectral")))
 p0fi
 
 p0qt<-ggplot.themap(Grid1km_LAU2_popEU[Grid1km_LAU2_popEU$Pop2021EU>thd,],
-                    "Pop2021EU",n=7)
+                    "Pop2021EU",n=7,
+                    cl.colours = rev(RColorBrewer::brewer.pal(7,"Spectral")))
 p0qt
 
 thd<-30 #threshold for no population
 p30qt<-ggplot.themap(Grid1km_LAU2_popEU[Grid1km_LAU2_popEU$Pop2021EU>thd,],
-                 "Pop2021EU",n=7)
+                 "Pop2021EU",n=7,
+                 cl.colours = rev(RColorBrewer::brewer.pal(7,"Spectral")))
 p30qt
 
 p30fi<-ggplot.themap(Grid1km_LAU2_popEU[Grid1km_LAU2_popEU$Pop2021EU>thd,],
-                     "Pop2021EU",n=7, style="fisher")
+                     "Pop2021EU",n=7, style="fisher",
+                     cl.colours = rev(RColorBrewer::brewer.pal(7,"Spectral")))
 p30fi
 
 
 #Build allocation factor (share) according to population
-agg_popEU_LAU2<-aggregate(Grid1km_LAU2_popEU$Pop2021EU, by=list(Grid1km_LAU2_popEU$maxm2LAU2), FUN=sum)
-names(agg_popEU_LAU2)<-c("LAU2","SumPop2021EU")
+agg_popEU_LAU2<-aggregate(list(SumPop2021EU=Grid1km_LAU2_popEU$Pop2021EU),
+                          by=list(LAU2=Grid1km_LAU2_popEU$maxm2LAU2), FUN=sum)
 
 merged2many<-merge(Grid1km_LAU2_popEU,agg_popEU_LAU2,
                   by.x="maxm2LAU2", by.y="LAU2", sort.x=FALSE, all.x=TRUE)
-merged2many$Pop2021EU_sh_of_LAU2<-merged_agg$Pop2021EU/merged_agg$SumPop2021EU
+merged2many$Pop2021EU_sh_of_LAU2<-merged2many$Pop2021EU/merged2many$SumPop2021EU
 
+#Reorder rows and rearrange columns
 Grid1km_LAU2_Pop2021EU<-merged2many[match(Grid1km_LAU2_popEU$CELLCODE,merged2many$CELLCODE),]
-
+Grid1km_LAU2_Pop2021EU<-Grid1km_LAU2_Pop2021EU[,c(2,1,3,4,5,6,8,7)]
 
 #Mapping correspondance shares
-pfi_sh<-ggplot.themap(Grid1km_LAU2_Pop2021EU,
-                     "Pop2021EU_sh_of_LAU2",n=7, style="fisher")
-pfi_sh
+pqt_sh<-ggplot.themap(Grid1km_LAU2_Pop2021EU[Grid1km_LAU2_Pop2021EU$Pop2021EU>0,],
+                     "Pop2021EU_sh_of_LAU2",n=7, style="quantile",
+                     cl.colours = rev(RColorBrewer::brewer.pal(7,"PiYG")),
+                     main.title = 'Each 1km grid cell contribution to commune',
+                     sub.title = "Based on Population 2021",
+                     leg.title = "Population sh. (qt)")
+pqt_sh
 
+# and overlay with communes
+LUX<-sf::st_read("data/Communes102_4326.gpkg")
+lucom3035<-sf::st_transform(LUX,crs=sf::st_crs(Grid1km_LAU2_Pop2021EU))
+
+pqt_sh_over<-pqt_sh+ggplot2::geom_sf(data=lucom3035, fill=NA)
+pqt_sh_over
 
 #save
 sf::st_write(Grid1km_LAU2_Pop2021EU,"data/Grid1km_LAU2_Pop2021EU.gpkg", delete_dsn=TRUE)
 write.csv(sf::st_drop_geometry(Grid1km_LAU2_Pop2021EU),"data/Grid1km_LAU2_Pop2021EU.csv")
 
 #print
+pdf("output/Lux_map_grid_pop.pdf")
+print(p0fi)
+print(p0qt)
+print(p30fi)
+print(p30qt)
+print(pfi_sh)
+print(pfi_sh_over)
+dev.off()
 
--- R/map.lux.grid.urban.R
+++ R/map.lux.grid.urban.R
@@ -38,11 +38,79 @@ artif18[artif18$code_2018=="11240","degree"]<-0.05
 
 # Intersecting with grid ----
 artif12_int<-sf::st_intersection(artif12, Grid1km_LAU2_Pop2021EU)
-artif18_int<-sf::st_intersection(artif18, Grid1km_LAU2_Pop2021EU)#Share of urban
+artif18_int<-sf::st_intersection(artif18, Grid1km_LAU2_Pop2021EU)
 
-artif12_int$surfartif <-sf::st_area(artif12_int)
+# Weighted urban surfaces per intersected----
+artif12_int$surfartif <-as.numeric(sf::st_area(artif12_int)) #as.numeric to remove units otherwise further calculations difficult
 artif12_int$surfdegree_m2 <- (artif12_int$surfartif*artif12_int$degree)
 
-artif18_int$surfartif <-sf::st_area(artif18_int)
+artif18_int$surfartif <-as.numeric(sf::st_area(artif18_int))#as.numeric to remove units otherwise further calculations difficult
 artif18_int$surfdegree_m2 <- (artif18_int$surfartif*artif18_int$degree)
 
+# Weighted urban surfaces per cell----
+Grid12_surfdegree_m2<-aggregate(list(SumSurf2012_m2=artif12_int$surfdegree_m2),
+                                by=list(CELLCODE=artif12_int$CELLCODE), FUN=sum )
+Grid18_surfdegree_m2<-aggregate(list(SumSurf2018_m2=artif18_int$surfdegree_m2),
+                                by=list(CELLCODE=artif18_int$CELLCODE), FUN=sum )
+Grid_SumSurf_m2<-merge(Grid12_surfdegree_m2,Grid18_surfdegree_m2,all=TRUE) #merge 2 years together
+
+#Merge to grid and replaces NA's by 0m2
+Grid1km_LAU2_Pop2021EU_Surf1218<-merge(Grid1km_LAU2_Pop2021EU,Grid_SumSurf_m2, all.x=TRUE)
+Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2012_m2[is.na(Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2012_m2)]<-0
+Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2[is.na(Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2)]<-0
+
+
+#Build allocation factor (share) according to urban surface
+# ! given allocation of each cell to a single commune based on total surface !
+
+agg_SumSurf_LAU2<-aggregate(list(LAU2Surf2012_m2=Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2012_m2,
+                                 LAU2Surf2018_m2=Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2),
+                            by=list(LAU2=Grid1km_LAU2_Pop2021EU_Surf1218$maxm2LAU2), FUN=sum)
+
+merged2many<-merge(Grid1km_LAU2_Pop2021EU_Surf1218,agg_SumSurf_LAU2,
+                   by.x="maxm2LAU2", by.y="LAU2", sort.x=FALSE, all.x=TRUE)
+merged2many$Urban2012_sh_of_LAU2<-merged2many$SumSurf2012_m2/merged2many$LAU2Surf2012_m2
+merged2many$Urban2018_sh_of_LAU2<-merged2many$SumSurf2018_m2/merged2many$LAU2Surf2018_m2
+
+
+
+#Reorder rows and rearrange columns
+Grid1km_LAU2_Pop2021EU_Surf1218<-merged2many[match(Grid1km_LAU2_Pop2021EU_Surf1218$CELLCODE,merged2many$CELLCODE),]
+Grid1km_LAU2_Pop2021EU_Surf1218<-Grid1km_LAU2_Pop2021EU_Surf1218[,c(2,1,3:11,13,14,12)]
+
+#Mapping correspondance shares
+pqt_sh_urb<-ggplot.themap(Grid1km_LAU2_Pop2021EU_Surf1218[Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2>0,],
+                      "Urban2018_sh_of_LAU2",n=7, style="quantile",
+                      cl.colours = rev(RColorBrewer::brewer.pal(7,"PiYG")),
+                      main.title = 'Each 1km grid cell contribution to commune',
+                      sub.title = "Based on Urbanised surfaces 2021",
+                      leg.title = "Urban 2018 sh. (qt)")
+pqt_sh_urb
+
+# and overlay with communes
+LUX<-sf::st_read("data/Communes102_4326.gpkg")
+lucom3035<-sf::st_transform(LUX,crs=sf::st_crs(Grid1km_LAU2_Pop2021EU_Surf1218))
+
+pqt_sh_urb_over<-pqt_sh_urb+ggplot2::geom_sf(data=lucom3035, fill=NA)
+pqt_sh_urb_over
+
+#Mapping net density or land artifi per person 
+Grid1km_LAU2_Pop2021EU_Surf1218$artifLand_capita18<-
+   Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2/Grid1km_LAU2_Pop2021EU_Surf1218$Pop2021EU
+
+subset1<-Grid1km_LAU2_Pop2021EU_Surf1218[(Grid1km_LAU2_Pop2021EU_Surf1218$Pop2021EU>0) & (Grid1km_LAU2_Pop2021EU_Surf1218$SumSurf2018_m2>0),]
+artifLand_capita<-ggplot.themap(subset1,"artifLand_capita18",
+                                n=7, style="quantile",
+                                cl.colours = rev(RColorBrewer::brewer.pal(7,"Spectral")),
+                          main.title = 'Artificialised residential land per capita 2021',
+                          leg.title = "m2/inh.")
+artifLand_capita
+
+artifLand_capita_over<-artifLand_capita+
+  ggplot2::geom_sf(data=lucom3035, fill=NA)
+artifLand_capita_over
+
+#printing
+pdf("output/Lux_map_grid_urban.pdf")
+print(pqt_sh_urb_over)
+dev.off()
