1243d59dba553e53238165256e9714c4c6b70bc0
geocaruso <geoffrey.caruso@gmail.com>
2024-05-13 15:02:40 +0200

    more general writing of internal distnce function. Plus adds a convex hull option

--- R/internal_distances.R
+++ R/internal_distances.R
@@ -1,25 +1,46 @@
-#The script computes the max distance from any point within each
-#municipality. It depends on the simplification level used for digitizing
-# since it computes the max distance after pairing all coordinates making
-# a polygon.
-# For use in other contexts, a simplify of polygons may be required beforehand.
+#The script computes the max distance from any point within each polygon of an sf.
+#Value and computing time depend on the simplification level (resolution)
+#used for digitizing since it computes the max distance after pairing
+# all coordinates making the convex hull of a polygon.
+# If convex hull is switched to FALSE then it computes it pairs all coordinates
+#  of each original polygon
+#For large territories or high resolution contexts, a simplify of polygons
+# may still be required beforehand.
+# Example is made for Luxembourg municipalities (102 communes).
+# In this case with or without convex hull makes little difference but the
+# use of convHull==TRUE adds a toll. Hence it is defaulted to false.
 
+#Function that compute max internal distance, plus NS and SE ranges for comparison
+intern.distances<-function(sf,idname,convHull=FALSE){
+#sf must be a polygon
+#idname is colname of unique id
+idlst<-sf::st_drop_geometry(sf)[,idname]
+
+list_distances<-lapply(idlst,function(x){
+i<-sf[sf::st_drop_geometry(sf)[,idname]==x,]
+if(convHull==TRUE) {
+  i<-sf::st_convex_hull(i)
+}
+
+bbi<-sf::st_bbox(i)
+NSdistance<-round(as.numeric(bbi[4]-bbi[2]))
+EWdistance<-round(as.numeric(bbi[3]-bbi[1]))
+coords_i<-sf::st_coordinates(i[1,])[,1:2]
+Maxdistance<-round(max(dist(coords_i)))
+return(cbind(NSdistance,EWdistance,Maxdistance))
+}
+)
+
+df_internal_distance<-cbind(sf::st_drop_geometry(sf),
+      do.call(rbind,list_distances))
+return(df_internal_distance)
+}
+
+#Example:
 #Get lat long Lux 102 communes and reproject into LUREF coordinates system (meters)
 lux102_4326<-sf::st_read("data/Communes102_4326.gpkg")
 lux102_2169<-sf::st_transform(lux102_4326, crs=2169)
 
-#Compute max plus NS and SE ranges
-list_distances<-lapply(lux102_2169$LAU2,function(x){
-i<-lux102_2169[lux102_2169$LAU2==x,]
-bbi<-sf::st_bbox(i)
-NSdistance<-as.numeric(bbi[4]-bbi[2])
-EWdistance<-as.numeric(bbi[3]-bbi[1])
-coords_i<-sf::st_coordinates(i[1,])[,1:2]
-Maxdistance<-max(dist(coords_i))
-return(cbind(NSdistance,EWdistance,Maxdistance))
-}
-)
-lux102_internal_distance<-cbind(sf::st_drop_geometry(lux102_2169),
-      do.call(rbind,list_distances))
+lux102_internal_distance<-intern.distances(lux102_2169,"LAU2")
 
 write.csv(lux102_internal_distance,"data/lux102_internal_distance.csv")
